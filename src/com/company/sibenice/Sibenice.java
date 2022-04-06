28f9ea6e96b13db39312a8c1bc70be5761bcc44c
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2022-04-06 12:44:00 +0200

    kritické změny

--- src/com/company/sibenice/Sibenice.java
+++ src/com/company/sibenice/Sibenice.java
@@ -2,6 +2,7 @@ package com.company.sibenice;
 
 import java.util.ArrayList;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,59 +10,12 @@ public class Sibenice {
     public static void Dead(){
         Scanner letter = new Scanner(System.in);
 
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-        int e = 0;
-        int f = 0;
-        int g = 0;
-        int h = 0;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int l = 0;
-        int m = 0;
-        int n = 0;
-        int o = 0;
-        int p = 0;
-        int q = 0;
-        int r = 0;
-        int s = 0;
-        int t = 0;
-        int u = 0;
-        int v = 0;
-        int w = 0;
-        int x = 0;
-        int y = 0;
-        int z = 0;
+        int[] abc = new int[27];
+        Character[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-        int ax = 0;
-        int bx = 0;
-        int cx = 0;
-        int dx = 0;
-        int ex = 0;
-        int fx = 0;
-        int gx = 0;
-        int hx = 0;
-        int ix = 0;
-        int jx = 0;
-        int kx = 0;
-        int lx = 0;
-        int mx = 0;
-        int nx = 0;
-        int ox = 0;
-        int px = 0;
-        int qx = 0;
-        int rx = 0;
-        int sx = 0;
-        int tx = 0;
-        int ux = 0;
-        int vx = 0;
-        int wx = 0;
-        int xx = 0;
-        int yx = 0;
-        int zx = 0;
+        ArrayList<Character> abeceda = new ArrayList<Character>(Arrays.asList(alphabet));
+
+        int[] abcx = new int[27];
 
         ArrayList<String> slova = new ArrayList<String>();
         slova.add("robot");
@@ -85,8 +39,10 @@ public class Sibenice {
         }
 
         for (int au = 0; au < rozklad.size(); au++){
-            if (rozklad.get(au).charValue()){
-                a++;
+            for (int ou = 0;ou < abeceda.size();ou++){
+                if (rozklad.get(au) == abeceda.get(ou)) {
+                    abc[ou]++;
+                }
             }
         }
     }
