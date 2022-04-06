f286aec233d1779c6ee2fcd1a909b4edf146e651
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2022-04-06 13:34:02 +0200

    skoro funguje
    Vypisování písmen na pozici nefunguje

--- src/com/company/sibenice/Sibenice.java
+++ src/com/company/sibenice/Sibenice.java
@@ -9,28 +9,31 @@ import java.util.Scanner;
 public class Sibenice {
     public static void Dead(){
         Scanner letter = new Scanner(System.in);
+        boolean hra  = true;
+        boolean vyhra  = false;
 
         int[] abc = new int[27];
+        for (int i = 0; i < 27; i++){
+            abc[i] = 0;
+        }
         Character[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         ArrayList<Character> abeceda = new ArrayList<Character>(Arrays.asList(alphabet));
 
         int[] abcx = new int[27];
+        for (int i = 0; i < 27; i++){
+            abcx[i] = 0;
+        }
 
         ArrayList<String> slova = new ArrayList<String>();
         slova.add("robot");
-        slova.add("sesit");
-        slova.add("lahev");
-        slova.add("cepice");
-        slova.add("mobil");
-        slova.add("pocitac");
-        slova.add("prodluzovak");
-        slova.add("kabel");
+
 
         Random slovo = new Random();
-        String hadanka = slova.get(slovo.nextInt(8));
+        String hadanka = slova.get(slovo.nextInt(1));
 
         int chyby = 0;
+        int existuje = 0;
 
         ArrayList<Character> rozklad = new ArrayList<Character>();
         for (int ou = 0; ou < hadanka.length(); ou++) {
@@ -45,5 +48,70 @@ public class Sibenice {
                 }
             }
         }
+
+        System.out.println("Jdeme hrát");
+        System.out.println("Zde máš podlážky");
+        for (int oi = 0; oi < rozklad.size(); oi++){
+            if (abcx[oi] == 0){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(abeceda.get(oi + 1));
+            }
+        }
+
+        while(hra) {
+            if (chyby < 10) {
+                System.out.println("");
+                System.out.println("Vypište písmeno");
+                String input = letter.next();
+                char vlozeno = input.charAt(0);
+                int pozice = 0;
+
+                boolean cekani = true;
+                int poiu = 0;
+                while (cekani) {
+                    if (vlozeno == abeceda.get(poiu)) {
+                        pozice = poiu;
+                        cekani = false;
+                    } else {
+                        poiu++;
+                    }
+                }
+                if (abc[pozice] > 0) {
+                    System.out.println("Písmeno je ve slově");
+                    abcx[pozice] += 1;
+                    existuje++;
+
+                    if (existuje == rozklad.size()){
+                        hra = false;
+                        vyhra = true;
+                    }
+                } else {
+                    System.out.println("Chyba");
+                    chyby++;
+                }
+                System.out.println("Počet chyb: " + chyby + "/10");
+                System.out.println("");
+                for (int oi = 0; oi < rozklad.size(); oi++) {
+                    if (abcx[oi] == 0) {
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(abeceda.get(oi));
+                    }
+                }
+
+            }
+            else {
+                System.out.println("");
+                System.out.println("Prohrál si");
+                hra = false;
+            }
+        }
+        if (vyhra){
+            System.out.println("Gratuluji");
+            System.out.println("Vyhrál si");
+        }
+        }
     }
-}
